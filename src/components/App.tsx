4f1725e50d5911200486898447555e264e37b7c0
Kevin Schans <kevinvdsd@hotmail.com>
2023-05-22 22:06:03 +0200

    Reorganize folders

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+
+import '/src/index.css'
+
+export function App() {
+  const title = 'The pointing gentlemen';
+
+  return (
+    <>
+      <Header title={title}></Header>
+    
+    </>
+
+  )
+}
